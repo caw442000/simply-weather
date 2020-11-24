65c80c16f2c96721d2b9a73dc372444be25e9d45
cedric-winbush <cawinbushjr@gmail.com>
2020-11-24 13:46:31 -0600

    adds basic header search pulls current data from api

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Zip from 'react-zipcode';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+// implement yup for validation
+// move to formik
+
+
+const Header = () => {
+  const [search, setSearch] = useState({
+    zipcode: "",
+    city: "",
+  })
+
+  const handleChange = e => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value
+    })
+    console.log("zip", search)
+  }
+
+  const formSubmit = (e) => {
+      e.preventDefault();
+
+      console.log("zip", search)
+      axiosWithAuth()
+      .get(`current.json?key=${API_KEY}&q=${search.zipcode}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+  return (
+    <div>
+      <form onSubmit={formSubmit}>
+        <input
+          type="text"
+          name="zipcode"
+          value={search.zipcode}
+          onChange={handleChange}
+        />
+          {/* <Zip onValue={(value) => setZip(value)} /> */}
+
+
+      </form>
+      <button>Click</button>
+      <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"/></a>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+export const axiosWithAuth = () => {
+  return axios.create({
+      baseURL: `http://api.weatherapi.com/v1/`,
+      headers: {
+      }
+  });
+}
+
+// export const axiosWithAuth = (search) => {
+//   return axios.create({
+//       baseURL: `http://api.weatherapi.com/v1/${search}/${API_KEY}`,
+//       headers: {
+//       }
+//   });
+// }
