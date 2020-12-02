0a717dad6b62a27db5512c4a709237d1038c9e9d
cedric-winbush <cawinbushjr@gmail.com>
2020-12-02 12:51:10 -0600

    adds title in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import GoogleSearch from './search/GoogleSearch';
 const Header = () => {
   return (
     <div className="header">
+      <h1>Simply Weather</h1>
       <GoogleSearch />
     </div>
   )
